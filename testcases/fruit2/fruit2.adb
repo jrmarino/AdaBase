63c56b1b7b05006fae0267140f4cb2dfdfeec01c
John Marino <draco@marino.st>
2016-04-24 10:32:09 +0200

    Add new testcase fruit2
    This demonstrates the driver version of last_sql_state, last_driver_code,
    last_driver_message, set_trait_error_mode, rollback and connections
    using an intentionally bad query string.

---
+++ testcases/fruit2/fruit2.adb
@@ -0,0 +1,43 @@
+with AdaBase;
+with Connect;
+with Ada.Text_IO;
+
+procedure Fruit2 is
+
+   package CON renames Connect;
+   package TIO renames Ada.Text_IO;
+
+   numrows : AdaBase.AffectedRows;
+
+   --  Intentionally broken UPDATE command (calories misspelling)
+   cmd : constant String := "UPDATE fruits set caloriesx = 14 " &
+                            "WHERE fruit = 'strawberry'";
+
+begin
+
+   declare
+   begin
+      CON.connect_database;
+      CON.DR.set_trait_error_mode (trait => AdaBase.raise_exception);
+   exception
+      when others =>
+         TIO.Put_Line ("database connect failed.");
+         return;
+   end;
+
+   TIO.Put_Line ("SQL: " & cmd);
+   declare
+   begin
+      numrows := CON.DR.execute (sql => cmd);
+      TIO.Put_Line ("Result: Updated" & numrows'Img & " rows");
+      CON.DR.rollback;
+   exception
+      when others =>
+         TIO.Put_Line ("Error!");
+         TIO.Put_Line ("Driver message: " & CON.DR.last_driver_message);
+         TIO.Put_Line ("   Driver code: " & CON.DR.last_driver_code'Img);
+         TIO.Put_Line ("     SQL State: " & CON.DR.last_sql_state);
+   end;
+   CON.DR.disconnect;
+
+end Fruit2;
