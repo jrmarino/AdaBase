9ce3e453a4ebc83f829a8db736bc167b48e7b934
John Marino <draco@marino.st>
2016-04-25 16:09:07 +0200

    Add new testcase ds_fetch
    This is the first testcase involving a statement and thus it tests
    many aspects of statements and fields.  It is a direct statement
    (not a prepared statement) and does not use binding or assignments.
    That's for another testcase.

--- testcases/driver_connections/mysql/connect.ads
+++ testcases/driver_connections/mysql/connect.ads
@@ -1,13 +1,17 @@
 --  Used for all testcases for MySQL driver
 
 with AdaBase.Driver.Base.MySQL;
+with AdaBase.Statement.Base.MySQL;
 
 package Connect is
 
    --  All specific drivers renamed to "Database_Driver"
+   --  All specific statement Accesses renamed to "Stmt_Access"
    subtype Database_Driver is AdaBase.Driver.Base.MySQL.MySQL_Driver;
+   subtype Stmt_Access is AdaBase.Statement.Base.MySQL.MySQL_statement_access;
 
    DR : Database_Driver;
+   STMT : Stmt_Access;
 
    procedure connect_database;
 
---
+++ testcases/ds_fetch/ds_fetch.adb
@@ -0,0 +1,61 @@
+with AdaBase;
+with Connect;
+with CommonText;
+with Ada.Text_IO;
+with AdaBase.Results.Sets;
+
+procedure DS_Fetch is
+
+   package CON renames Connect;
+   package TIO renames Ada.Text_IO;
+   package ARS renames AdaBase.Results.Sets;
+   package CT  renames CommonText;
+
+   row : ARS.DataRow_Access;
+   sql : constant String := "SELECT * FROM fruits WHERE color = 'orange'";
+
+begin
+
+   declare
+   begin
+      CON.connect_database;
+   exception
+      when others =>
+         TIO.Put_Line ("database connect failed.");
+         return;
+   end;
+
+   CON.STMT := CON.DR.query (sql);
+
+   TIO.Put_Line (" Query successful: " & CON.STMT.successful'Img);
+   TIO.Put_Line (" Discard possible: " & CON.STMT.discards_possible'Img);
+   TIO.Put_Line ("Number of columns:" & CON.STMT.column_count'Img);
+   TIO.Put_Line ("   Number of rows:" & CON.STMT.rows_returned'Img);
+
+   TIO.Put_Line ("");
+   for c in Natural range 1 .. CON.STMT.column_count loop
+      TIO.Put_Line ("Column" & c'Img & ":");
+      TIO.Put_Line ("   TABLE: " & CON.STMT.column_table (c));
+      TIO.Put_Line ("    NAME: " & CON.STMT.column_name (c));
+      TIO.Put_Line ("    TYPE: " & CON.STMT.column_native_type (c)'Img);
+   end loop;
+
+   TIO.Put_Line ("");
+   loop
+      row := CON.STMT.fetch_next;
+      exit when ARS.complete (row);
+      TIO.Put (CT.zeropad (Natural (row.column (1).as_byte2), 2) & " ");
+      declare
+         fruit : String := row.column ("fruit").as_string;
+         frlen : Natural := fruit'Length;
+         rest  : String (1 .. 12 - frlen) := (others => ' ');
+      begin
+         TIO.Put (rest & fruit);
+      end;
+      TIO.Put (" (" & row.column ("color").as_string & ") calories =");
+      TIO.Put_Line (row.column (4).as_byte2'Img);
+   end loop;
+
+   CON.DR.disconnect;
+
+end DS_Fetch;
