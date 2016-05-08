e4295aa552bc7e4e0b17571e2f9dbcd829726a52
John Marino <draco@marino.st>
2016-05-08 22:32:53 +0200

    fix query_select test case

--- testcases/prep_stmt/prep_stmt.adb
+++ testcases/prep_stmt/prep_stmt.adb
@@ -18,7 +18,6 @@ begin
 
    CON.DR.command_standard_logger (device => ALF.screen, action => ALF.attach);
 
-   declare
    begin
       CON.connect_database;
    exception
@@ -30,26 +29,26 @@ begin
    declare
       max_calories : aliased AR.byte2 := 200;
       min_calories : constant AR.byte2 := 5;
-      row : ARS.DataRow_Access;
-   begin
-      CON.STMT := CON.DR.prepare_select
+      row          : ARS.DataRow;
+      stmt : CON.Stmt_Type := CON.DR.prepare_select
         (tables     => "fruits",
          columns    => "*",
          conditions => "color = ? and calories > :mincal and calories < ?");
+   begin
+      stmt.assign (1, "red");
+      stmt.assign ("mincal", min_calories);
+      stmt.assign (3, max_calories'Unchecked_Access);
 
-      CON.STMT.assign (1, "red");
-      CON.STMT.assign ("mincal", min_calories);
-      CON.STMT.assign (3, max_calories'Unchecked_Access);
-
-      if CON.STMT.execute then
+      if stmt.execute then
          TIO.Put_Line ("execute succeeded");
-         for c in Natural range 1 .. CON.STMT.column_count loop
+         for c in Natural range 1 .. stmt.column_count loop
             TIO.Put_Line ("Column" & c'Img & " heading: " &
-                          CON.STMT.column_name (c));
+                          stmt.column_name (c));
          end loop;
-         TIO.Put_Line ("returned rows: " & CON.STMT.rows_returned'Img);
+         TIO.Put_Line ("returned rows: " & stmt.rows_returned'Img);
          loop
-            exit when not CON.STMT.fetch_next (row);
+            row := stmt.fetch_next;
+            exit when row.data_exhausted;
             TIO.Put_Line (row.column (2).as_string & " (" &
                           row.column ("color").as_string & ") " &
                           row.column ("calories").as_string  & " calories");
@@ -62,11 +61,11 @@ begin
    declare
       sql : String := "INSERT INTO fruits (fruit, color, calories) " &
                       "VALUES ('potato','tan', 77)";
+      stmt : CON.Stmt_Type := CON.DR.prepare (sql);
    begin
-      CON.STMT := CON.DR.prepare (sql);
-      if CON.STMT.execute then
-         TIO.Put_Line ("Inserted row " & CON.STMT.last_insert_id'Img);
-         TIO.Put_Line ("Affected rows: " & CON.STMT.rows_affected'Img);
+      if stmt.execute then
+         TIO.Put_Line ("Inserted row " & stmt.last_insert_id'Img);
+         TIO.Put_Line ("Affected rows: " & stmt.rows_affected'Img);
       end if;
       CON.DR.rollback;
    end;
--- testcases/query_select/query_select.adb
+++ testcases/query_select/query_select.adb
@@ -14,7 +14,6 @@ procedure Query_Select is
 
 begin
 
-   declare
    begin
       CON.connect_database;
    exception
@@ -25,7 +24,8 @@ begin
 
    CON.DR.set_trait_column_case (AdaBase.upper_case);
 
-   CON.STMT := CON.DR.query_select
+   declare
+      stmt : CON.Stmt_Type := CON.DR.query_select
                (tables    => "nhl_schedule as S " &
                              "JOIN nhl_teams T1 ON S.home_team = T1.team_id " &
                              "JOIN nhl_teams T2 ON S.away_team = T2.team_id",
@@ -43,62 +43,65 @@ begin
                limit      => 10,
                offset     => 20);
 
-   if not CON.STMT.successful then
-      TIO.Put_Line ("  Driver message: " & CON.STMT.last_driver_message);
-      TIO.Put_Line ("     Driver code: " & CON.STMT.last_driver_code'Img);
-      TIO.Put_Line ("       SQL State: " & CON.STMT.last_sql_state);
-   else
-      for c in Natural range 1 .. CON.STMT.column_count loop
-         TIO.Put_Line ("Column" & c'Img & " heading: " &
-                       CON.STMT.column_name (c));
-      end loop;
-      TIO.Put_Line ("");
-   end if;
-
-   --  Demonstrate bind/fetch_bound
-   declare
-      event_code : aliased AR.nbyte2;
-      home_town, home_mascot  : aliased AR.textual;
-      away_town, away_mascot  : aliased AR.textual;
-      home_score, away_score  : aliased AR.nbyte1;
    begin
-      CON.STMT.bind (1, event_code'Unchecked_Access);
-      CON.STMT.bind ("HOME_CITY", home_town'Unchecked_Access);
-      CON.STMT.bind ("AWAY_CITY", away_town'Unchecked_Access);
-      CON.STMT.bind (3, home_mascot'Unchecked_Access);
-      CON.STMT.bind ("AWAY_MASCOT", away_mascot'Unchecked_Access);
-      CON.STMT.bind ("HOME_SCORE", home_score'Unchecked_Access);
-      CON.STMT.bind ("AWAY_SCORE", away_score'Unchecked_Access);
+      if not stmt.successful then
+         TIO.Put_Line ("  Driver message: " & stmt.last_driver_message);
+         TIO.Put_Line ("     Driver code: " & stmt.last_driver_code'Img);
+         TIO.Put_Line ("       SQL State: " & stmt.last_sql_state);
+      else
+         for c in Natural range 1 .. stmt.column_count loop
+            TIO.Put_Line ("Column" & c'Img & " heading: " &
+                          stmt.column_name (c));
+         end loop;
+         TIO.Put_Line ("");
+      end if;
 
-      loop
-         exit when not CON.STMT.fetch_bound;
+      --  Demonstrate bind/fetch_bound
+      declare
+         event_code : aliased AR.nbyte2;
+         home_town, home_mascot  : aliased AR.textual;
+         away_town, away_mascot  : aliased AR.textual;
+         home_score, away_score  : aliased AR.nbyte1;
+      begin
+         stmt.bind (1, event_code'Unchecked_Access);
+         stmt.bind ("HOME_CITY", home_town'Unchecked_Access);
+         stmt.bind ("AWAY_CITY", away_town'Unchecked_Access);
+         stmt.bind (3, home_mascot'Unchecked_Access);
+         stmt.bind ("AWAY_MASCOT", away_mascot'Unchecked_Access);
+         stmt.bind ("HOME_SCORE", home_score'Unchecked_Access);
+         stmt.bind ("AWAY_SCORE", away_score'Unchecked_Access);
 
-         TIO.Put ("In event" & event_code'Img & ", the " &
-                  CT.USS (away_town) & " " & CT.USS (away_mascot) &
-                  " visited the " &
-                  CT.USS (home_town) & " " & CT.USS (home_mascot) & " and ");
-         if Integer (away_score) > Integer (home_score) then
-            TIO.Put ("won");
-         elsif Integer (away_score) < Integer (home_score) then
-            TIO.Put ("lost");
-         else
-            TIO.Put ("tied");
-         end if;
-         TIO.Put_Line (away_score'Img & " to" & home_score'Img);
-      end loop;
-      TIO.Put_Line ("");
+         loop
+            exit when not stmt.fetch_bound;
+
+            TIO.Put ("In event" & event_code'Img & ", the " &
+                     CT.USS (away_town) & " " & CT.USS (away_mascot) &
+                     " visited the " &
+                     CT.USS (home_town) & " " & CT.USS (home_mascot) &
+                     " and ");
+            if Integer (away_score) > Integer (home_score) then
+               TIO.Put ("won");
+            elsif Integer (away_score) < Integer (home_score) then
+               TIO.Put ("lost");
+            else
+               TIO.Put ("tied");
+            end if;
+            TIO.Put_Line (away_score'Img & " to" & home_score'Img);
+         end loop;
+         TIO.Put_Line ("");
+      end;
    end;
 
-   CON.STMT := CON.DR.query_select
+   declare
+      --  demonstrate fetch_all
+      stmt : CON.Stmt_Type := CON.DR.query_select
                (tables    => "fruits",
                columns    => "fruit, calories, color",
                conditions => "calories > 50",
                order      => "calories",
                limit      => 10);
 
-   --  demonstrate fetch_all
-   declare
-      rowset : ARS.DataRowSet := CON.STMT.fetch_all;
+      rowset : ARS.DataRowSet := stmt.fetch_all;
    begin
       for row in Natural range 1 .. rowset'Length loop
          TIO.Put_Line (rowset (row).column (1).as_string & ":" &
