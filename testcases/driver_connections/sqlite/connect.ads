7c472e27311c5f9b2c2d4b35d06a5ba027b62dea
John Marino <draco@marino.st>
2016-05-11 19:59:32 +0200

    enable prep stmt support in sqlite testcases

--- testcases/driver_connections/sqlite/connect.ads
+++ testcases/driver_connections/sqlite/connect.ads
@@ -7,9 +7,9 @@ package Connect is
 
    --  All specific drivers renamed to "Database_Driver"
    subtype Database_Driver is AdaBase.Driver.Base.SQLite.SQLite_Driver;
---   subtype Stmt_Type is AdaBase.Statement.Base.SQLite.SQLite_statement;
---   subtype Stmt_Type_access is
---      AdaBase.Statement.Base.SQLite.SQLite_statement_access;
+   subtype Stmt_Type is AdaBase.Statement.Base.SQLite.SQLite_statement;
+   subtype Stmt_Type_access is
+      AdaBase.Statement.Base.SQLite.SQLite_statement_access;
 
    DR : Database_Driver;
 
