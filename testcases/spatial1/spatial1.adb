c1be64189c3651999c91db0b9a315889deae84ab
John Marino <draco@marino.st>
2016-05-30 15:32:55 +0200

    document full checklist for spatial testcase

--- testcases/spatial1/spatial1.adb
+++ testcases/spatial1/spatial1.adb
@@ -25,19 +25,50 @@ procedure Spatial1 is
    my_infline : Geometry := initialize_as_infinite_line (((0.0, 0.0), (2.0, 2.0)));
 
 begin   
+
+   --  critical checklist
+   --  =========================================================
+   --  single point                                           59
+   --  single line (converts to line string)                  60
+   --  single infinite line (!)                               64
+   --  single line_string                                     61
+   --  single polygon                                         62
+   --  single polygon with 1 hole                          88-91
+   --  single polygon with 2 holes                         92-95
+   --  single circle (!)                                      63
+   --  point + point = multipoint                          73-75
+   --  point + line_string = collection
+   --  point + polygon = collection
+   --  point + polygon with 1 hole = 2 item collection
+   --  point + polygon hole = EXCEPTION
+   --  line_string + line_string = multiline               77-78
+   --  line_string + point + point = collection
+   --  line_string + polygon = collection
+   --  line_string + polygon + hole = 2 item collection
+   --  polygon + point = 2 item collection
+   --  polygon + line_string = 2 item collection
+   --  polygon + hole + point + point = 3 item collection
+   --  polygon + hole + polygon = 2 item multipolygon
+   --  polygon + hole + polygon + point = 3 item collection
+   --  polygon + polygon + hole = 2 item multipolygon
+   --  polygon + polygon + hole + line_string = 3 item collection
+   --  polygon + hole + hole + polygon + hole = 2 item multipolygon
+   --  polygon + hole + point = 2 item collection
+   --  polygon + hole + line_string = 2 item collection
+
    print (my_point,   "SINGLE POINT");
    print (my_line,    "SINGLE LINE");
    print (my_linestr, "SINGLE LINE STRING (THREE POINT)");
    print (my_polygon, "SINGLE POLYGON (3 SIDES)");
    print (my_circle,  "SINGLE CIRCLE (not legal for MySQL or WKT)");
    print (my_infline, "INFINITE LINE (converted to regular line on MySQL or WKT)");
-
-   --  convert my point to to a multi-point collection
    
    declare
       pt1 : Geometric_point := (9.2, 4.773);
       pt2 : Geometric_point := (-7.01, -4.9234);
       pt3 : Geometric_point := (4.5, 6.0);
+      polyhole1 : Geometry;
+      polyhole2 : Geometry;
    begin
       append_point (my_point, pt1);
       append_point (my_point, pt2);
@@ -55,7 +86,12 @@ begin
       print (my_line, "MIXED COLLECTION #2");
 
       append_polygon_hole  (my_polygon, ((1.0, 2.0), (3.2, 4.5), (8.8, 7.7), (1.0, 2.0)));
-      print (my_polygon, "STILL SINGLE POLYGON");
+      polyhole1 := my_polygon;
+      print (polyhole1, "STILL SINGLE POLYGON #1");
+
+      polyhole2 := polyhole1;
+      append_polygon_hole (polyhole2, ((13.5, 15.35), (98.1, 11.7), (-13.75, 0.0004), (13.5, 15.35)));
+      print (polyhole2, "STILL SINGLE POLYGON #2");
 
       append_polygon (my_polygon, ((5.0, 6.0), (1.4, 2.2), (18.1, 24.0), (5.0, 6.0)));
       print (my_polygon, "POLYGON COLLECTION #1");
