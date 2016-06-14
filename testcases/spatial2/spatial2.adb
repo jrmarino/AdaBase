f50beac0173f88c7344e340d52c7ecf3f3df23da
jrmarino <draco@marino.st>
2016-06-14 14:38:57 +0200

    Restore spatial2 test case
    I had to recreate this as it was overwritten accidently.  There's still
    a geo-in-geo case to write.

--- testcases/spatial2/spatial2.adb
+++ testcases/spatial2/spatial2.adb
@@ -16,9 +16,25 @@ begin
    CON.connect_database;
 
    declare
-      sql  : constant String := "SELECT sp_point, sp_linestring, " &
-                                "ST_AsText (sp_point) as point2 " &
-                                "FROM spatial_plus";
+      sql  : constant String :=
+         "SELECT" &
+            "  sp_point" &
+            ", ST_AsText (sp_point) as tx_point" &
+            ", sp_linestring" &
+            ", ST_AsText (sp_linestring) as tx_linestring" &
+            ", sp_multi_point" &
+            ", ST_AsText (sp_multi_point) as tx_multi_point" &
+            ", sp_multi_line_string" &
+            ", ST_AsText (sp_multi_line_string) as tx_multi_line_string" &
+            ", sp_polygon" &
+            ", ST_AsText (sp_polygon) as tx_polygon" &
+            ", sp_outer_ring" &
+            ", ST_AsText (sp_outer_ring) as tx_outer_ring" &
+            ", sp_multi_polygon" &
+            ", ST_AsText (sp_multi_polygon) as tx_multi_polygon" &
+            ", sp_geo_collection" &
+            ", ST_AsText (sp_geo_collection) as tx_geo_collection" &
+         " FROM spatial_plus";
       stmt : CON.Stmt_Type := CON.DR.query (sql);
       row  : ARS.Datarow;
    begin
@@ -30,10 +46,10 @@ begin
          row := stmt.fetch_next;
          exit when row.data_exhausted;
          for x in Natural range 1 .. row.count loop
-            TIO.Put ("column : ");
+            TIO.Put ("    column : ");
             TIO.Put (stmt.column_name (x));
-            TIO.Put (" : " & row.column (x).native_type'Img);
-            TIO.Put_Line (" : " & row.column (x).as_string);
+            TIO.Put_Line (" : " & row.column (x).native_type'Img);
+            TIO.Put_Line (row.column (x).as_string);
          end loop;
       end loop;
    end;
